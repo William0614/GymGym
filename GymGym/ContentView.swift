32f9a88fc45b62ecb2f4306de3a129dca0f4da2e
William0614 <65125377+William0614@users.noreply.github.com>
2024-08-20 14:50:21 +0900

    Initial Commit

---
+++ GymGym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GymGym
+//
+//  Created by 김보윤 on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GymGym/GymGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymGymApp.swift
+//  GymGym
+//
+//  Created by 김보윤 on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GymGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
